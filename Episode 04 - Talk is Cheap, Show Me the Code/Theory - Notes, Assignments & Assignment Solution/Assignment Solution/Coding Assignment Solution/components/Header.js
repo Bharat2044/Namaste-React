0798f342d10b991f074fb86a3c42e46d2e0fea49
Bharat2044 <bk9955204456@gmail.com>
2024-06-10 05:28:18 +0530

    Episode 04 - Talk is Cheap, Show Me the Code coding assignment solution added

---
+++ Episode 04 - Talk is Cheap, Show Me the Code/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/components/Header.js
@@ -0,0 +1,23 @@
+import tastyTrailsLogo from "../../../../../public/images/tasty-trails-logo.png";
+import { FaCartArrowDown } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={tastyTrailsLogo} alt="Tasty Trails Logo" />
+      </div>
+      
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li><FaCartArrowDown /></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
