fd85be4f83eb9b76b410a555bbe080ed0c752654
Bharat2044 <bk9955204456@gmail.com>
2024-06-10 05:28:07 +0530

    Episode 04 - Talk is Cheap, Show Me the Code coding assignment solution added

---
+++ Episode 04 - Talk is Cheap, Show Me the Code/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/components/AppLayout.js
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import Header from "./Header";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default AppLayout;
