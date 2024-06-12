58dae86cc0fc1efba7ec6e9475fdc07c8cd493ed
Bharat2044 <bk9955204456@gmail.com>
2024-06-12 13:09:12 +0530

    coding assignment solution added

---
+++ Episode 06 - Exploring the World/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/src/components/Body.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import {
+  SWIGGY_API_URL,
+  SWIGGY_RESTAURANT_PATH,
+} from "../../../../../../public/common/constants";
+
+const Body = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [searchRestaurant, setSearchRestaurant] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [restaurantName, setRestaurantName] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch(SWIGGY_API_URL);
+      const json = await data.json();
+      const restaurants = eval("json?." + SWIGGY_RESTAURANT_PATH) || [];
+
+      setRestaurantList(restaurants);
+      setFilteredRestaurants(restaurants);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSearch = () => {
+    const filtered = restaurantList.filter((res) =>
+      res.info.name.toLowerCase().includes(searchRestaurant.toLowerCase())
+    );
+
+    setFilteredRestaurants(filtered);
+    setSearchRestaurant(""); // Clear the search input box after search
+    setRestaurantName(searchRestaurant);
+  };
+
+  // Conditional rendering using ternary operator
+  return restaurantList.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="search-box">
+        <input
+          type="text"
+          value={searchRestaurant}
+          onChange={(e) => setSearchRestaurant(e.target.value)}
+          placeholder="search a restaurant you want..."
+        />
+        <button className="search" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+
+      <div className="restaurant-container">
+        {filteredRestaurants.length !== 0 ? (
+          filteredRestaurants.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant?.info?.id}
+              {...restaurant?.info}
+            />
+          ))
+        ) : (
+          <h2>Sorry, we couldn't find any restaurant for "{restaurantName}"</h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
