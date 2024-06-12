c2d7f82f831f3e6c291e7a2c31b8d3c3deb6d717
Bharat2044 <bk9955204456@gmail.com>
2024-06-12 10:53:45 +0530

    code added

---
+++ Episode 06 - Exploring the World/Coding/src/components/Shimmer.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Shimmer() {
+  return (
+    <div className="body">
+      <div className="shimmer-search-box">
+      </div>
+      <div className="restaurant-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Shimmer;
