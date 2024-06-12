8160bce8f37a90e4eb1f777317431bed2943a975
Bharat2044 <bk9955204456@gmail.com>
2024-06-12 10:53:07 +0530

    swiggy api link added

---
+++ public/common/constants.js
@@ -0,0 +1,20 @@
+// Swiggy Image CDN URL for Restaurant card
+export const IMAGE_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+// Swiggy API for to get Restaurant data
+export const SWIGGY_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+// Swiggy Restaurant Path 
+export const SWIGGY_RESTAURANT_PATH = "data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants"
+
+
+// Social Media Links - URL
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/bharat2044/";
+export const GiTHUB_LINK = "https://github.com/Bharat2044";
+export const TWITTER_LINK = "https://x.com/bharat__2044/";
+export const EMAIL_LINK = "mailto:bharatkumar204451@gmal.com";
+
+
+// Github - username and repository name
+export const Github_UserName = "Bharat2044";
+export const Github_Repository_Name = "Namaste-React";
