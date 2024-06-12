9f4025d5493d1fce0df9a533290769f14034a413
Bharat2044 <bk9955204456@gmail.com>
2024-06-12 10:53:41 +0530

    code added

---
+++ Episode 06 - Exploring the World/Coding/src/components/RestaurantCard.js
@@ -0,0 +1,50 @@
+import { MdStarRate } from "react-icons/md";
+import { IMAGE_CDN_URL } from "../../../../public/common/constants";
+
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  areaName,
+  sla,
+  cuisines,
+  costForTwo,
+  avgRating,
+}) => {
+  return (
+    <div className="restaurant-card">
+      <img
+        src={IMAGE_CDN_URL + cloudinaryImageId}
+        alt={name}
+        className="restaurant-logo"
+      />
+      <div className="restaurant-details">
+        <h3 className="restaurant-name">
+          {name.slice(0, 22)}
+          {name.length > 22 ? "..." : ""}
+        </h3>
+        <div className="esa-rating">
+          <h4 className="rating">
+            <MdStarRate
+              className="rating-logo"
+              style={
+                avgRating > 4.2
+                  ? { backgroundColor: "var(--green)" }
+                  : { backgroundColor: "var(--red)" }
+              }
+            />
+            <span>{avgRating}</span>
+          </h4>
+          <h4>{costForTwo}</h4>
+          <h4>{sla.deliveryTime} mins</h4>
+        </div>
+        <p className="cousine">
+          {cuisines.join(", ").slice(0, 30)}
+          {cuisines.join(", ").length > 30 ? "..." : ""}
+        </p>
+        <p className="location">{areaName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCard;
