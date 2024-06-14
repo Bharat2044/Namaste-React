c8248b2ae20dc8dec75b91788d05e84cb97b2579
Bharat2044 <bk9955204456@gmail.com>
2024-06-14 11:03:33 +0530

    api path resolued

--- public/common/constants.js
+++ public/common/constants.js
@@ -2,19 +2,13 @@
 export const SWIGGY_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
 //  Swiggy API for to get Restaurant Item 
-export const RES_ITEM_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-
-// Restaurant Image CDN URL for Restaurant card
-export const RES_IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
+export const RES_API_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9046136&lng=77.614948&restaurantId=42150&catalog_qa=undefined&submitAction=ENTER`;
 
 // Restaurant Item Image CDN URL for Restaurant card
-export const RES_ITEM_IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
+export const IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/`;
 
 // Swiggy Restaurant Path 
 export const SWIGGY_REST_API_PATH = `data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants`;
-
-// Swiggy Restaurant Items Path 
-export const SWIGGY_REST_ITEM_API_PATH = `data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants`;
 
 
 // Social Media Links - URL
