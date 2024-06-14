6bcf1412bf601bd773460afceb2303150dbf6219
Bharat2044 <bk9955204456@gmail.com>
2024-06-14 16:10:58 +0530

    swiggy api url added

--- public/common/constants.js
+++ public/common/constants.js
@@ -2,7 +2,7 @@
 export const SWIGGY_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
 //  Swiggy API for to get Restaurant Item 
-export const RES_API_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9046136&lng=77.614948&restaurantId=42150&catalog_qa=undefined&submitAction=ENTER`;
+export const MENU_API_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9046136&lng=77.614948&restaurantId=`;
 
 // Restaurant Item Image CDN URL for Restaurant card
 export const IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/`;
