2568d3bf288a2eabcd26cfca7d236b8355b1213d
Bharat2044 <bk9955204456@gmail.com>
2024-07-02 13:21:40 +0530

    code added

---
+++ Episode 09 - Optimizing our App/Coding/src/components/RestaurantCard.js
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import { IMG_CDN_URL } from "../../../../public/common/constants";
+import { MdStarRate } from "react-icons/md";
+import "../styles/RestaurantCard.css";
+
+const RestaurantCard = ({
+  id,
+  cloudinaryImageId,
+  name,
+  areaName,
+  sla,
+  cuisines,
+  costForTwo,
+  avgRatingString,
+}) => {
+  return (
+    <Link to={"/restaurants/" + id} className="restaurant-card">
+      <img
+        src={IMG_CDN_URL + cloudinaryImageId}
+        alt={name}
+        className="restaurant-image"
+      />
+      <div className="restaurant-details">
+        <h3 className="restaurant-name">
+          {name.length > 23 ? name.slice(0, 20) + "..." : name.slice(0, 24)}
+        </h3>
+        <div className="esa-rating">
+          <h4 className="rating">
+            <MdStarRate
+              className="rating-logo"
+              style={
+                avgRatingString > 4.0
+                  ? { backgroundColor: "var(--green)" }
+                  : { backgroundColor: "var(--red)" }
+              }
+            />
+            <span>{avgRatingString}</span>
+          </h4>
+          <h4>{costForTwo}</h4>
+          <h4>{sla.deliveryTime} mins</h4>
+        </div>
+        <p className="cousine">
+          {cuisines.join(", ").length > 32
+            ? cuisines.join(", ").slice(0, 28) + "..."
+            : cuisines.join(", ").slice(0, 32)}
+        </p>
+        <p className="location">{areaName}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default RestaurantCard;
