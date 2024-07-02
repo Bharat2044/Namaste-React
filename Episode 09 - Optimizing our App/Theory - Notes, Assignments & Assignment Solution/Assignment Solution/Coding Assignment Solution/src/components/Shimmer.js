cf1f1ea0325065c54e3ca55a42eede641dcd7847
Bharat2044 <bk9955204456@gmail.com>
2024-07-02 14:31:42 +0530

    coding assignment solution added

---
+++ Episode 09 - Optimizing our App/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/src/components/Shimmer.js
@@ -0,0 +1,42 @@
+export const RestaurantShimmer = () => {
+  return (
+    <div className="body">
+      <div className="shimmer-search-box"></div>
+      <div className="restaurant-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    </div>
+  );
+};
+
+export const RestaurantMenuShimmer = () => {
+  return (
+    <div className="shimmer-menu">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+};
