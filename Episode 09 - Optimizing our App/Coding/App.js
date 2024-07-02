f4c53ed85f74bfc5e0023cf2dea6186eac63ca61
Bharat2044 <bk9955204456@gmail.com>
2024-07-02 13:20:24 +0530

    code added

--- Episode 09 - Optimizing our App/Coding/App.js
+++ Episode 09 - Optimizing our App/Coding/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import "./index.css";
 import Header from "./src/components/Header";
-import Body from "./src/components/Body";
+// import Body from "./src/components/Body";
 import Footer from "./src/components/Footer";
-import About from "./src/components/About";
+// import About from "./src/components/About";
 import Contact from "./src/components/Contact";
 import Error from "./src/components/Error";
 import RestaurantMenu from "./src/components/RestaurantMenu";
 import Login from "./src/components/Login";
+import { RestaurantShimmer } from "./src/components/Shimmer";
+// import Grocery from "./src/components/Grocery";
+
+/**
+ * Chunking
+ * Lazy Loading
+ * Code Splitting
+ * Dynamic Imports
+ * Dynamic Bundding
+ * Prefetching
+ * Suspense
+ * On-Demand Loading
+ */
+const Grocery = lazy(() => import("./src/components/Grocery"));
+const About = lazy(() => import("./src/components/About"));
+const Body = lazy(() => import("./src/components/Body"));
 
 const App = () => {
   return (
@@ -29,15 +45,31 @@ const appRouter = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Body />,
+        element: (
+          <Suspense fallback={<RestaurantShimmer />}>
+            <Body />
+          </Suspense>
+        ),
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
