a16a9ba950d958469c130d204794e47f74a633e6
Bharat2044 <bk9955204456@gmail.com>
2024-07-02 13:21:01 +0530

    code added

---
+++ Episode 09 - Optimizing our App/Coding/src/components/Grocery.js
@@ -0,0 +1,14 @@
+const Grocery = () => {
+  return (
+    <h1 style={{
+        width: '90%',
+        textAlign: 'center',
+        color: 'purple',        
+    }}>
+      Our Grocery Online Store, and we have a lot of child components inside
+      this web page!!
+    </h1>
+  );
+};
+
+export default Grocery;
