29158092843c5d53756bf58d384032f0cbb20bee
Bharat2044 <bk9955204456@gmail.com>
2024-07-02 14:30:37 +0530

    coding assignment solution added

---
+++ Episode 09 - Optimizing our App/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/App.js
@@ -0,0 +1,82 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./index.css";
+import Header from "./src/components/Header";
+// import Body from "./src/components/Body";
+import Footer from "./src/components/Footer";
+// import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import Login from "./src/components/Login";
+import { RestaurantShimmer } from "./src/components/Shimmer";
+
+/**
+ * Chunking
+ * Lazy Loading
+ * Code Splitting
+ * Dynamic Imports
+ * Dynamic Bundding
+ * Prefetching
+ * Suspense
+ * On-Demand Loading
+ */
+const About = lazy(() => import("./src/components/About"));
+const Body = lazy(() => import("./src/components/Body"));
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+// Children Routes
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<RestaurantShimmer />}>
+            <Body />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/hello",
+    element: <h1>Hello, World!!</h1>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
