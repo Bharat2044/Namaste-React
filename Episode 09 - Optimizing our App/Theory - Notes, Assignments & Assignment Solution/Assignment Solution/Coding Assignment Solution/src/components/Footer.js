cc4fdd7910fe268b4f43403693df2c411417c6d5
Bharat2044 <bk9955204456@gmail.com>
2024-07-02 14:31:10 +0530

    coding assignment solution added

---
+++ Episode 09 - Optimizing our App/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/src/components/Footer.js
@@ -0,0 +1,22 @@
+import {LINKEDIN_LINK} from "../../../../../../public/common/constants";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      Created By
+      <span>❤️</span>
+      <a className="linkedin-name" href={LINKEDIN_LINK} target="_blank">
+        Bharat Kumar
+      </a>
+      <span>&copy;</span>
+      {year}
+      <strong>
+        Tasty <span>Trails</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
