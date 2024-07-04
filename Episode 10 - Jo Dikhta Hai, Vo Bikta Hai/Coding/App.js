65965e58ea4954ed4bec75cada09da559dbbcec6
Bharat2044 <bk9955204456@gmail.com>
2024-07-04 22:54:22 +0530

    code added

--- Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/Coding/App.js
+++ Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/Coding/App.js
@@ -1,17 +1,14 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import "./index.css";
 import Header from "./src/components/Header";
-// import Body from "./src/components/Body";
 import Footer from "./src/components/Footer";
-// import About from "./src/components/About";
 import Contact from "./src/components/Contact";
 import Error from "./src/components/Error";
 import RestaurantMenu from "./src/components/RestaurantMenu";
 import Login from "./src/components/Login";
 import { RestaurantShimmer } from "./src/components/Shimmer";
-// import Grocery from "./src/components/Grocery";
+import "./index.css";
 
 /**
  * Chunking
@@ -23,13 +20,13 @@ import { RestaurantShimmer } from "./src/components/Shimmer";
  * Suspense
  * On-Demand Loading
  */
-const Grocery = lazy(() => import("./src/components/Grocery"));
+
 const About = lazy(() => import("./src/components/About"));
 const Body = lazy(() => import("./src/components/Body"));
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app w-full flex flex-col justify-between items-center mt-[120px] min-h-[calc(100vh-120px)]">
       <Header />
       <Outlet />
       <Footer />
@@ -62,14 +59,6 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
-      },
-      {
-        path: "/grocery",
-        element: (
-          <Suspense fallback={<h1>Loading...</h1>}>
-            <Grocery />
-          </Suspense>
-        ),
       },
       {
         path: "/restaurants/:resId",
