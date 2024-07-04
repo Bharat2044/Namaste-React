d8cf972598f15ee1fd2c23c1c12aa745ab7977d3
Bharat2044 <bk9955204456@gmail.com>
2024-07-04 21:00:28 +0530

    tailwind file added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    // "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
