ebe04236389026f20d1da61c062f33083ab928d6
Bharat2044 <bk9955204456@gmail.com>
2024-07-05 19:44:28 +0530

    code added

---
+++ Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/Coding/src/components/RestaurantMenu.js
@@ -0,0 +1,106 @@
+import { IMG_CDN_URL } from "../../../../public/common/constants";
+import { RestaurantMenuShimmer } from "./Shimmer";
+import { MdStarRate } from "react-icons/md";
+import { useParams } from "react-router-dom";
+import "../styles/RestaurantMenu.css";
+import useRestaurantMenuData from "../hooks/useRestaurantMenuData";
+
+const RestaurantMenu = () => {
+  const { resId } = useParams();
+  const restaurantInfo = useRestaurantMenuData(resId);
+
+  if (restaurantInfo === null) {
+    return <RestaurantMenuShimmer />;
+  }
+
+  const {
+    cloudinaryImageId,
+    name,
+    avgRatingString,
+    totalRatingsString,
+    cuisines,
+    locality,
+    sla,
+  } = restaurantInfo?.cards[2]?.card?.card?.info || {};
+
+  const cards =
+    restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards || [];
+
+  let itemCards =
+    cards.find((c) => c?.card?.card?.itemCards)?.card?.card?.itemCards || [];
+
+  return (
+    <div className="menu">
+      <div className="restaurant-header">
+        <img src={IMG_CDN_URL + cloudinaryImageId} alt={name} />
+        <div className="restaurant-header-details">
+          <h1>{name}</h1>
+          <h3>{locality}</h3>
+          <p>{cuisines?.join(", ")}</p>
+          <h4 className="rating-time">
+            <div className="rating">
+              <MdStarRate
+                className="rating-logo"
+                style={{
+                  backgroundColor:
+                    avgRatingString >= 4.0 ? "var(--green)" : "var(--red)",
+                }}
+              />
+              <span>
+                {avgRatingString || 3.8} ({totalRatingsString || "1K+ ratings"})
+              </span>
+            </div>
+            <span>|</span>
+            <span className="time">{sla?.slaString}</span>
+          </h4>
+        </div>
+      </div>
+
+      {itemCards.length ? (
+        itemCards.map((item) => {
+          const {
+            id,
+            name,
+            price,
+            defaultPrice,
+            ratings,
+            imageId,
+            description,
+          } = item.card.info;
+          return (
+            <div key={id} className="menu-items">
+              <div className="left">
+                <h2>{name}</h2>
+                <h4>₹{price / 100 || defaultPrice / 100}</h4>
+                <p>{description && description.slice(0, 60) || "Dummy Data"}</p>
+                <h4 className="rating">
+                  <MdStarRate
+                    className="rating-logo"
+                    style={{
+                      backgroundColor:
+                        ratings?.aggregatedRating?.rating >= 4.0
+                          ? "var(--green)"
+                          : "var(--red)",
+                    }}
+                  />
+                  <span>
+                    {ratings?.aggregatedRating?.rating || 3.8} (
+                    {ratings?.aggregatedRating?.ratingCountV2 || 6})
+                  </span>
+                </h4>
+              </div>
+              <div className="right">
+                <img src={IMG_CDN_URL + imageId} alt={name} />
+                <button className="add-btn">ADD</button>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <h2>No items available</h2>
+      )}
+    </div>
+  );
+};
+
+export default RestaurantMenu;
