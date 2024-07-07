8902255b47dd64699e328f3ec3aedb78a312a63c
Bharat2044 <bk9955204456@gmail.com>
2024-07-07 21:40:20 +0530

    code added

---
+++ Episode 11 - Data is the New Oil/Coding/App.js
@@ -0,0 +1,81 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./src/components/Header";
+import Footer from "./src/components/Footer";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import Login from "./src/components/Login";
+import { RestaurantShimmer } from "./src/components/Shimmer";
+import "./index.css";
+
+/**
+ * Chunking
+ * Lazy Loading
+ * Code Splitting
+ * Dynamic Imports
+ * Dynamic Bundding
+ * Prefetching
+ * Suspense
+ * On-Demand Loading
+ */
+
+const About = lazy(() => import("./src/components/About"));
+const Body = lazy(() => import("./src/components/Body"));
+
+const App = () => {
+  return (
+    <div className="w-full flex flex-col justify-between items-center mt-[120px] min-h-[calc(100vh-120px)]">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+// Children Routes
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<RestaurantShimmer />}>
+            <Body />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1 className="text-3xl font-bold">Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/hello",
+    element: <h1 className="text-3xl font-bold">Hello, World!!</h1>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
