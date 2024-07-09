424dc46ddb0647582aa7adad9a0ef218e36e2095
Bharat2044 <bk9955204456@gmail.com>
2024-07-10 01:33:45 +0530

    code added

--- Episode 11 - Data is the New Oil/Coding/src/components/Header.js
+++ Episode 11 - Data is the New Oil/Coding/src/components/Header.js
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import tastyTrailsLogo from "../../../../public/images/tasty-trails-logo.png";
 import useOnlineStatus from "../hooks/useOnlineStatus";
 import { FaCartArrowDown } from "react-icons/fa";
+import UserContext from "../context/UserContext";
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const navigate = useNavigate();
-
   const isOnline = useOnlineStatus();
+  const { loggedInUser } = useContext(UserContext);
 
   return (
     <div className="w-screen h-20 flex justify-between items-center text-[#545454] px-6 py-0 bg-[#ffffff] shadow-[-2px_7px_5px_-6px_#0000009c] font-bold fixed top-0 left-0 z-[999]">
@@ -22,7 +23,10 @@ const Header = () => {
         </Link>
       </div>
 
+
       <div>{isOnline ? "✅ Online" : "❌ Disconnected"}</div>
+
+      <div>{loggedInUser ? loggedInUser.slice(0, 20) : ""}</div>
 
       <div>
         <ul className="list-none flex items-center">
@@ -53,22 +57,23 @@ const Header = () => {
           <li className="p-2 cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]">
             <FaCartArrowDown />
           </li>
-
-          {isLoggedIn ? (
-            <button
-              className="px-2 py-[6px] cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
-              onClick={() => setIsLoggedIn(false)}
-            >
-              Logout
-            </button>
-          ) : (
-            <button
-              className="px-2 py-[6px] cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
-              onClick={() => navigate("/login")}
-            >
-              Login
-            </button>
-          )}
+          <li>
+            {isLoggedIn ? (
+              <button
+                className="px-2 py-[6px] cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+                onClick={() => setIsLoggedIn(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button
+                className="px-2 py-[6px] cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+            )}
+          </li>
         </ul>
       </div>
     </div>
