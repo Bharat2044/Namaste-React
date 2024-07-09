cee445459c56687b3ff2a21981924ccd921190c2
Bharat2044 <bk9955204456@gmail.com>
2024-07-09 22:30:31 +0530

    code added

---
+++ Episode 11 - Data is the New Oil/Coding/src/components/Shimmer.js
@@ -0,0 +1,42 @@
+export const RestaurantShimmer = () => {
+  return (
+    <div className="w-[90%] flex flex-col items-center transition-all">
+      <div className="w-[50%] h-[50px] bg-[#cecccc] rounded-lg mb-[30px]"></div>
+      <div className="flex flex-wrap justify-center items-center gap-x-[1.5%] gap-y-[25px]">
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+        <div className="w-[250px] h-[300px] bg-[#cecccc] rounded-[8px]"></div>
+      </div>
+    </div>
+  );
+};
+
+export const RestaurantMenuShimmer = () => {
+  return (
+    <div className="w-[60%] flex flex-wrap items-center justify-center gap-[30px]">
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+      <div className="w-[100%] h-[200px] bg-[#cecccc] rounded-[8px]"></div>
+    </div>
+  );
+};
