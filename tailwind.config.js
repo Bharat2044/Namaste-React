760eb9a3f1618d43b23651d45e89cf54fe95b001
Bharat2044 <bk9955204456@gmail.com>
2024-07-10 16:47:20 +0530

    tailwind file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     // "./src/**/*.{html,js,ts,jsx,tsx}",
     "./Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/**/*.{html,js,ts,jsx,tsx}",
     "./Episode 11 - Data is the New Oil/**/*.{html,js,ts,jsx,tsx}",
+    "./Episode 12 - Let's Build Our Store/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
