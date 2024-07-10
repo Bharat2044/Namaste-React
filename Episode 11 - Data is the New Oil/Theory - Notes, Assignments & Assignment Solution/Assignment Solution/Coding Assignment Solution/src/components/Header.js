98c6c9e2342518a808e855ce579491eda6fcd2c6
Bharat2044 <bk9955204456@gmail.com>
2024-07-10 15:46:25 +0530

    coding assignment solution added

---
+++ Episode 11 - Data is the New Oil/Theory - Notes, Assignments & Assignment Solution/Assignment Solution/Coding Assignment Solution/src/components/Header.js
@@ -0,0 +1,83 @@
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import tastyTrailsLogo from "../../../../../../public/images/tasty-trails-logo.png";
+import useOnlineStatus from "../hooks/useOnlineStatus";
+import { FaCartArrowDown } from "react-icons/fa";
+import UserContext from "../context/UserContext";
+
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const navigate = useNavigate();
+  const isOnline = useOnlineStatus();
+  const { loggedInUser } = useContext(UserContext);
+
+  return (
+    <div className="w-screen h-20 flex justify-between items-center text-[#545454] px-6 py-0 bg-[#ffffff] shadow-[-2px_7px_5px_-6px_#0000009c] font-bold fixed top-0 left-0 z-[999]">
+      <div>
+        <Link to="/">
+          <img
+            className="w-14 h-14 rounded-full cursor-pointer"
+            src={tastyTrailsLogo}
+            alt="Tasty Trails Logo"
+          />
+        </Link>
+      </div>
+
+
+      <div>{isOnline ? "✅ Online" : "❌ Disconnected"}</div>
+
+      <div>{loggedInUser ? loggedInUser.slice(0, 20) : ""}</div>
+
+      <div>
+        <ul className="list-none flex items-center">
+          <li>
+            <Link
+              className="p-2 cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="p-2 cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="p-2 cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+              to="/contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="p-2 cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]">
+            <FaCartArrowDown />
+          </li>
+          <li>
+            {isLoggedIn ? (
+              <button
+                className="px-2 py-[6px] cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+                onClick={() => setIsLoggedIn(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button
+                className="px-2 py-[6px] cursor-pointer text-inherit hover:text-white hover:bg-[#E46F20] hover:rounded-[5px]"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
