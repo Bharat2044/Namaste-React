9cddbdc61b33ff1ab9de9cb44bcccefc52a67e55
Bharat2044 <bk9955204456@gmail.com>
2024-07-13 07:57:57 +0530

    code added

---
+++ Episode 12 - Lets Build Our Store/Coding/src/hooks/useRestaurantMenuData.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { MENU_API_URL } from "../../../../public/common/constants";
+
+const useRestaurantMenuData = (resId) => {
+  const [restaurantInfo, setRestaurantInfo] = useState(null);
+
+  const fetchResMenusData = async () => {
+    try {
+      const response = await fetch(MENU_API_URL + resId);
+      const json = await response.json();
+
+      setRestaurantInfo(json?.data);
+    } catch (error) {
+      console.error("Error while fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchResMenusData();
+  }, []);
+
+  return restaurantInfo;
+};
+
+export default useRestaurantMenuData;
