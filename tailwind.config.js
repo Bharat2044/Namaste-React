27c78847d63c7e60f50dc18fddd0a2538f004fa3
Bharat2044 <bk9955204456@gmail.com>
2024-07-13 07:52:26 +0530

    tailwind file configured updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@
 module.exports = {
   content: [
     // "./src/**/*.{html,js,ts,jsx,tsx}",
-    "./Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/**/*.{html,js,ts,jsx,tsx}",
-    "./Episode 11 - Data is the New Oil/**/*.{html,js,ts,jsx,tsx}",
-    "./Episode 12 - Let's Build Our Store/**/*.{html,js,ts,jsx,tsx}",
+    // "./Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/**/*.{html,js,ts,jsx,tsx}",
+    // "./Episode 11 - Data is the New Oil/**/*.{html,js,ts,jsx,tsx}",
+    "./Episode 12 - Lets Build Our Store/**/*.{html,js,ts,jsx,tsx}", // Note: If you use Let's(' or `) then it won't work
   ],
   theme: {
     extend: {},
