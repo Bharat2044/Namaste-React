262e2f9016ec461ac5a991dac702e1080a806489
Bharat2044 <bk9955204456@gmail.com>
2024-07-13 07:58:05 +0530

    code added

---
+++ Episode 12 - Lets Build Our Store/Coding/src/hooks/useRestaurantData.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import {
+  SWIGGY_API_URL,
+  SWIGGY_REST_API_PATH,
+} from "../../../../public/common/constants";
+
+const useRestaurantData = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  const fetchRestaurantsData = async () => {
+    try {
+      const response = await fetch(SWIGGY_API_URL);
+      const json = await response.json();
+      const restaurants = eval("json?." + SWIGGY_REST_API_PATH) || [];
+
+      setListOfRestaurants(restaurants);
+      setFilteredRestaurants(restaurants);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurantsData();
+  }, []);
+
+  return [listOfRestaurants, filteredRestaurants, setFilteredRestaurants];
+};
+
+export default useRestaurantData;
